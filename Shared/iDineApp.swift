b503a6c70bebcb0732a14b051f96cde9164a9497
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2022-10-12 13:21:03 +0300

    Initial Commit

---
+++ Shared/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  Shared
+//
+//  Created by dennis mugambi on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
